325dc122ca0747efa077c77133e6e939261d554b
phuongmt@nexondv.com <phuongmt@nexondv.com>
2023-05-04 09:52:27 +0700

    test

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,27 +12,6 @@ namespace net
   /// <summary>
   /// Interaction logic for MainWindow.xaml
   /// </summary>
-  public partial class MyService : ServiceBase
-  {
-    private Thread _thread;
-    private net.MainWindow _wpfApp;
-
-    protected override void OnStart(string[] args)
-    {
-      _thread = new Thread(() =>
-      {
-        _wpfApp = new net.MainWindow();
-        _wpfApp.Run();
-      });
-      _thread.Start();
-    }
-
-    protected override void OnStop()
-    {
-      _wpfApp.Shutdown();
-      _thread.Join();
-    }
-  }
 
 
   public partial class MainWindow : Window
